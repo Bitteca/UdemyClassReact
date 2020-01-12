2c0bf1048210da072cf16982ac77cfefaa66c4c1
Bitteca <biteca@gmail.com>
2020-01-12 02:41:41 -0300

    Finalizada sessao sobre "State"

---
+++ src/SeasonDisplay/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const SeasonDisplay = () => <div>Season Display</div>;
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import { Container } from './styles';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: null,
+      errorMessage: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getUserLatitude();
+  }
+
+  getUserLatitude() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ latitude: position.coords.latitude }),
+      err =>
+        this.setState({
+          errorMessage: `Something went wrong. Please Allow Geolocation. Error Message: ${err.message}`,
+        })
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (this.state.latitude && !this.state.errorMessage) {
+      return <div> Latitude: {this.state.latitude} </div>;
+    }
+    return <div>Loading...</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
